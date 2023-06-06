ea003e44a5df5bf1bf45f1c65b398940f482fa9d
jaepaek123 <36895383+jaepaek123@users.noreply.github.com>
2023-06-06 13:01:48 -0700

    Use KMS Key ID as the primary key, instead of the 'JWT' + KID (#3)

--- packages/handlers/jwks/test/index.test.ts
+++ packages/handlers/jwks/test/index.test.ts
@@ -5,7 +5,7 @@ import { handler } from '../src/index';
 import { Context } from 'aws-lambda';
 import {
     DynamoDBClient,
-    ScanCommand,
+    GetItemCommand,
     PutItemCommand,
 } from '@aws-sdk/client-dynamodb';
 import {
@@ -25,14 +25,14 @@ describe('jwks.json Tests', () => {
     it('Found JWK, TTL > 10 days', async () => {
         const entry = data.jwksValidEntry(30);
 
-        ddbMock.on(ScanCommand).resolves(entry);
+        ddbMock.on(GetItemCommand).resolves(entry);
 
         const response = await handler(data.jwksEventRequest, {} as Context, (error, result) => {/* do nothing */ });
-        expect(ddbMock).toHaveReceivedCommandTimes(ScanCommand, 1);
+        expect(ddbMock).toHaveReceivedCommandTimes(GetItemCommand, 1);
         expect(kmsMock).toHaveReceivedCommandTimes(PutItemCommand, 0);
         expect(kmsMock).toHaveReceivedCommandTimes(GetPublicKeyCommand, 0);
         expect(response?.statusCode).toBe(200);
-        expect(JSON.parse(response?.body!).keys[0].kid).toBe(entry.Items![0]['kid']['S']);
+        expect(JSON.parse(response?.body!).keys[0].kid).toBe(entry.Item!['kid']['S']);
     });
 
     it('Found JWK, TTL < 10 days', async () => {
@@ -40,31 +40,31 @@ describe('jwks.json Tests', () => {
         const refreshEntry = data.jwksRefreshEntry(30);
 
         ddbMock
-            .on(ScanCommand).resolvesOnce(entry).resolves(refreshEntry)
+            .on(GetItemCommand).resolvesOnce(entry).resolves(refreshEntry)
             .on(PutItemCommand).resolves(data.jwksPutItem);
         kmsMock.on(GetPublicKeyCommand).resolves(data.jwksGetPublicKey);
 
         const response = await handler(data.jwksEventRequest, {} as Context, (error, result) => {/* do nothing */ });
-        expect(ddbMock).toHaveReceivedCommandTimes(ScanCommand, 2);
+        expect(ddbMock).toHaveReceivedCommandTimes(GetItemCommand, 2);
         expect(ddbMock).toHaveReceivedCommandTimes(PutItemCommand, 1);
         expect(kmsMock).toHaveReceivedCommandTimes(GetPublicKeyCommand, 1);
         expect(response?.statusCode).toBe(200);
-        expect(JSON.parse(response?.body!).keys[0].kid).toBe(refreshEntry.Items![0]['kid']['S']);
+        expect(JSON.parse(response?.body!).keys[0].kid).toBe(refreshEntry.Item!['kid']['S']);
     });
 
     it('Not found JWK', async () => {
         const refreshEntry = data.jwksRefreshEntry(30);
 
         ddbMock
-            .on(ScanCommand).resolvesOnce(data.jwksEmptyEntry).resolves(refreshEntry)
+            .on(GetItemCommand).resolvesOnce(data.jwksEmptyEntry).resolves(refreshEntry)
             .on(PutItemCommand).resolves(data.jwksPutItem);
         kmsMock.on(GetPublicKeyCommand).resolves(data.jwksGetPublicKey);
 
         const response = await handler(data.jwksEventRequest, {} as Context, (error, result) => {/* do nothing */ });
-        expect(ddbMock).toHaveReceivedCommandTimes(ScanCommand, 2);
+        expect(ddbMock).toHaveReceivedCommandTimes(GetItemCommand, 2);
         expect(ddbMock).toHaveReceivedCommandTimes(PutItemCommand, 1);
         expect(kmsMock).toHaveReceivedCommandTimes(GetPublicKeyCommand, 1);
         expect(response?.statusCode).toBe(200);
-        expect(JSON.parse(response?.body!).keys[0].kid).toBe(refreshEntry.Items![0]['kid']['S']);
+        expect(JSON.parse(response?.body!).keys[0].kid).toBe(refreshEntry.Item!['kid']['S']);
     });
 });
