db3dc6ece7f1cdf00ce7f70303583deaed2b92f7
Avinash Kaza <akaza@amazon.com>
2023-08-14 12:53:19 -0400

    Increase lambda function memory so that we improve on latencies based on service testing in T&C, https://docs.aws.amazon.com/lambda/latest/operatorguide/computing-power.html

--- packages/cdk/src/constructs/lambdas.ts
+++ packages/cdk/src/constructs/lambdas.ts
@@ -1,37 +1,56 @@
-import { ArnFormat, Duration, Stack } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { Architecture, AssetCode, LayerVersion, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
-import path from 'path';
-import { RetentionDays } from 'aws-cdk-lib/aws-logs';
-import { Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import { Keys } from './keys';
-import { Tables } from './tables';
-import { NagSuppressions } from 'cdk-nag';
+import { ArnFormat, Duration, Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+  Architecture,
+  AssetCode,
+  LayerVersion,
+  Runtime,
+  Tracing,
+} from "aws-cdk-lib/aws-lambda";
+import path from "path";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import {
+  Effect,
+  ManagedPolicy,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { Keys } from "./keys";
+import { Tables } from "./tables";
+import { NagSuppressions } from "cdk-nag";
 
 export interface LambdasConfig {
   tables: Tables;
   keys: Keys;
 }
 
-const LOG_LEVEL = 'debug';
+const LOG_LEVEL = "debug";
 
-const ltiNodejsFunction = (scope: Construct, id: string, props?: NodejsFunctionProps): NodejsFunction => {
+const ltiNodejsFunction = (
+  scope: Construct,
+  id: string,
+  props?: NodejsFunctionProps
+): NodejsFunction => {
   const role = new Role(scope, `${id}Role`, {
-    assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
   });
 
   // create log retention role
   const logRetentionRole = new Role(scope, `${id}LogRetentionRole`, {
-    assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+    assumedBy: new ServicePrincipal("lambda.amazonaws.com"),
   });
 
   const nodejsFunction = new NodejsFunction(scope, id, {
-    memorySize: 256,
+    memorySize: 1536,
     architecture: Architecture.ARM_64,
     timeout: Duration.seconds(30),
     runtime: Runtime.NODEJS_18_X,
-    handler: 'handler',
+    handler: "handler",
     role: role,
     logRetentionRole: logRetentionRole,
     logRetention: RetentionDays.TEN_YEARS,
@@ -46,9 +65,9 @@ const ltiNodejsFunction = (scope: Construct, id: string, props?: NodejsFunctionP
       new PolicyStatement({
         effect: Effect.ALLOW,
         actions: [
-          'logs:CreateLogGroup',
-          'logs:CreateLogStream',
-          'logs:PutLogEvents',
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
         ],
         resources: [nodejsFunction.logGroup.logGroupArn],
       }),
@@ -57,41 +76,47 @@ const ltiNodejsFunction = (scope: Construct, id: string, props?: NodejsFunctionP
   });
 
   // tslint:disable-next-line
-  const logRetentionRolePolicy = new ManagedPolicy(scope, `${id}LogRetentionPolicy`, {
-    statements: [
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          'logs:CreateLogGroup',
-          'logs:PutRetentionPolicy',
-          'logs:DeleteRetentionPolicy',
-        ],
-        resources: [Stack.of(nodejsFunction).formatArn({
-          service: 'logs',
-          resource: 'log-group',
-          arnFormat: ArnFormat.COLON_RESOURCE_NAME,
-          resourceName: '/aws/lambda/*',
-        })],
-      }),
-    ],
-    roles: [logRetentionRole],
-  });
+  const logRetentionRolePolicy = new ManagedPolicy(
+    scope,
+    `${id}LogRetentionPolicy`,
+    {
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            "logs:CreateLogGroup",
+            "logs:PutRetentionPolicy",
+            "logs:DeleteRetentionPolicy",
+          ],
+          resources: [
+            Stack.of(nodejsFunction).formatArn({
+              service: "logs",
+              resource: "log-group",
+              arnFormat: ArnFormat.COLON_RESOURCE_NAME,
+              resourceName: "/aws/lambda/*",
+            }),
+          ],
+        }),
+      ],
+      roles: [logRetentionRole],
+    }
+  );
   nodejsFunction.node.addDependency(logRetentionRolePolicy);
 
   NagSuppressions.addResourceSuppressions(
     [role, logRetentionRole, rolePolicy, logRetentionRolePolicy],
     [
       {
-        id: 'AwsSolutions-IAM5',
-        reason: 'Suppress all AwsSolutions-IAM5 findings on ltiNodejsFunction role as required by log group.',
+        id: "AwsSolutions-IAM5",
+        reason:
+          "Suppress all AwsSolutions-IAM5 findings on ltiNodejsFunction role as required by log group.",
       },
     ],
     true
   );
 
   return nodejsFunction;
-}
-
+};
 
 export class Lambdas extends Construct {
   readonly oidc: NodejsFunction;
@@ -108,24 +133,26 @@ export class Lambdas extends Construct {
   constructor(scope: Construct, id: string, config: LambdasConfig) {
     super(scope, id);
 
-    const utilLayer = new LayerVersion(this, 'layerUtil', {
-      code: AssetCode.fromAsset(path.join(__dirname, '../../../../dist/layers/util')),
+    const utilLayer = new LayerVersion(this, "layerUtil", {
+      code: AssetCode.fromAsset(
+        path.join(__dirname, "../../../../dist/layers/util")
+      ),
       compatibleRuntimes: [Runtime.NODEJS_18_X],
-      description: 'LTI utility functions',
+      description: "LTI utility functions",
     });
 
-    this.oidc = ltiNodejsFunction(this, 'ltiOidc', {
+    this.oidc = ltiNodejsFunction(this, "ltiOidc", {
       entry: path.join(
         __dirname,
-        '../../../handlers/oidc/src/index.ts'.replace('/', path.sep)
+        "../../../handlers/oidc/src/index.ts".replace("/", path.sep)
       ),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
         STATE_TTL: Duration.hours(2).toSeconds().toString(), // Auto expire STATE records after two hours
-        POWERTOOLS_SERVICE_NAME: 'oidc',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "oidc",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -134,16 +161,16 @@ export class Lambdas extends Construct {
     config.tables.controlPlaneTable.grantReadData(this.oidc);
     config.tables.dataPlaneTable.grantReadWriteData(this.oidc);
 
-    this.launch = ltiNodejsFunction(this, 'ltiLaunch', {
-      entry: path.join(__dirname, '../../../handlers/launch/src/index.ts'),
+    this.launch = ltiNodejsFunction(this, "ltiLaunch", {
+      entry: path.join(__dirname, "../../../handlers/launch/src/index.ts"),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
 
         KMS_KEY_ID: config.keys.asymmetricKey.keyId,
-        POWERTOOLS_SERVICE_NAME: 'launch',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "launch",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -152,17 +179,17 @@ export class Lambdas extends Construct {
     config.tables.controlPlaneTable.grantReadWriteData(this.launch);
     config.tables.dataPlaneTable.grantReadWriteData(this.launch);
 
-    this.authorizerProxy = ltiNodejsFunction(this, 'authorizerProxy', {
+    this.authorizerProxy = ltiNodejsFunction(this, "authorizerProxy", {
       entry: path.join(
         __dirname,
-        '../../../handlers/authorizerProxy/src/index.ts'
+        "../../../handlers/authorizerProxy/src/index.ts"
       ),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
         KMS_KEY_ID: config.keys.asymmetricKey.keyId,
-        POWERTOOLS_SERVICE_NAME: 'authorizerProxy',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "authorizerProxy",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -170,14 +197,14 @@ export class Lambdas extends Construct {
     });
     config.tables.dataPlaneTable.grantReadWriteData(this.authorizerProxy);
 
-    this.tokenProxy = ltiNodejsFunction(this, 'tokenProxy', {
-      entry: path.join(__dirname, '../../../handlers/tokenProxy/src/index.ts'),
+    this.tokenProxy = ltiNodejsFunction(this, "tokenProxy", {
+      entry: path.join(__dirname, "../../../handlers/tokenProxy/src/index.ts"),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
         KMS_KEY_ID: config.keys.asymmetricKey.keyId,
-        POWERTOOLS_SERVICE_NAME: 'tokenProxy',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "tokenProxy",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -185,18 +212,18 @@ export class Lambdas extends Construct {
     });
     config.tables.dataPlaneTable.grantReadWriteData(this.tokenProxy);
 
-    this.scoreSubmission = ltiNodejsFunction(this, 'scoreSubmission', {
+    this.scoreSubmission = ltiNodejsFunction(this, "scoreSubmission", {
       entry: path.join(
         __dirname,
-        '../../../handlers/scoreSubmission/src/index.ts'
+        "../../../handlers/scoreSubmission/src/index.ts"
       ),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
         KMS_KEY_ID: config.keys.asymmetricKey.keyId,
-        POWERTOOLS_SERVICE_NAME: 'scoreSubmission',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "scoreSubmission",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -205,18 +232,18 @@ export class Lambdas extends Construct {
     config.tables.controlPlaneTable.grantReadData(this.scoreSubmission);
     config.tables.dataPlaneTable.grantReadWriteData(this.scoreSubmission);
 
-    this.rosterRetrieval = ltiNodejsFunction(this, 'rosterRetrieval', {
+    this.rosterRetrieval = ltiNodejsFunction(this, "rosterRetrieval", {
       entry: path.join(
         __dirname,
-        '../../../handlers/rosterRetrieval/src/index.ts'
+        "../../../handlers/rosterRetrieval/src/index.ts"
       ),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
         KMS_KEY_ID: config.keys.asymmetricKey.keyId,
-        POWERTOOLS_SERVICE_NAME: 'rosterRetrieval',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "rosterRetrieval",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -224,18 +251,18 @@ export class Lambdas extends Construct {
     });
     config.tables.controlPlaneTable.grantReadWriteData(this.rosterRetrieval);
 
-    this.deepLinkingProxy = ltiNodejsFunction(this, 'deepLinkingProxy', {
+    this.deepLinkingProxy = ltiNodejsFunction(this, "deepLinkingProxy", {
       entry: path.join(
         __dirname,
-        '../../../handlers/deepLinkingProxy/src/index.ts'
+        "../../../handlers/deepLinkingProxy/src/index.ts"
       ),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         DATA_PLANE_TABLE_NAME: config.tables.dataPlaneTable.tableName,
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
         KMS_KEY_ID: config.keys.asymmetricKey.keyId,
-        POWERTOOLS_SERVICE_NAME: 'deepLinkingProxy',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "deepLinkingProxy",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -244,13 +271,13 @@ export class Lambdas extends Construct {
     config.tables.controlPlaneTable.grantReadData(this.deepLinkingProxy);
     config.tables.dataPlaneTable.grantReadWriteData(this.deepLinkingProxy);
 
-    this.platform = ltiNodejsFunction(this, 'ltiPlatformRegister', {
-      entry: path.join(__dirname, '../../../handlers/platform/src/index.ts'),
+    this.platform = ltiNodejsFunction(this, "ltiPlatformRegister", {
+      entry: path.join(__dirname, "../../../handlers/platform/src/index.ts"),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
-        POWERTOOLS_SERVICE_NAME: 'platformRegister',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "platformRegister",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -258,13 +285,13 @@ export class Lambdas extends Construct {
     });
     config.tables.controlPlaneTable.grantWriteData(this.platform);
 
-    this.tool = ltiNodejsFunction(this, 'ltiToolConfig', {
-      entry: path.join(__dirname, '../../../handlers/tool/src/index.ts'),
+    this.tool = ltiNodejsFunction(this, "ltiToolConfig", {
+      entry: path.join(__dirname, "../../../handlers/tool/src/index.ts"),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
-        POWERTOOLS_SERVICE_NAME: 'toolConfig',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "toolConfig",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -272,14 +299,14 @@ export class Lambdas extends Construct {
     });
     config.tables.controlPlaneTable.grantWriteData(this.tool);
 
-    this.jwks = ltiNodejsFunction(this, 'ltiJwks', {
-      entry: path.join(__dirname, '../../../handlers/jwks/src/index.ts'),
+    this.jwks = ltiNodejsFunction(this, "ltiJwks", {
+      entry: path.join(__dirname, "../../../handlers/jwks/src/index.ts"),
       environment: {
         /* eslint-disable @typescript-eslint/naming-convention */
         CONTROL_PLANE_TABLE_NAME: config.tables.controlPlaneTable.tableName,
         KMS_KEY_ID: config.keys.asymmetricKey.keyId, // Auto expire STATE records after two hours
-        POWERTOOLS_SERVICE_NAME: 'jwks',
-        POWERTOOLS_METRICS_NAMESPACE: 'lti',
+        POWERTOOLS_SERVICE_NAME: "jwks",
+        POWERTOOLS_METRICS_NAMESPACE: "lti",
         LOG_LEVEL,
         /* eslint-enable */
       },
@@ -288,14 +315,18 @@ export class Lambdas extends Construct {
     config.tables.controlPlaneTable.grantReadWriteData(this.jwks);
 
     const asymmetricKeyGrant = new PolicyStatement({
-      actions: ['kms:Verify', 'kms:GetPublicKey', 'kms:Sign'],
+      actions: ["kms:Verify", "kms:GetPublicKey", "kms:Sign"],
       effect: Effect.ALLOW,
       resources: [config.keys.asymmetricKey.keyArn],
     });
     this.jwks.grantPrincipal.addToPrincipalPolicy(asymmetricKeyGrant);
     this.launch.grantPrincipal.addToPrincipalPolicy(asymmetricKeyGrant);
-    this.deepLinkingProxy.grantPrincipal.addToPrincipalPolicy(asymmetricKeyGrant);
-    this.scoreSubmission.grantPrincipal.addToPrincipalPolicy(asymmetricKeyGrant);
+    this.deepLinkingProxy.grantPrincipal.addToPrincipalPolicy(
+      asymmetricKeyGrant
+    );
+    this.scoreSubmission.grantPrincipal.addToPrincipalPolicy(
+      asymmetricKeyGrant
+    );
     this.rosterRetrieval.grantPrincipal.addToPrincipalPolicy(
       asymmetricKeyGrant
     );
